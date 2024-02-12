09dd932893441c479925b781cfeb85175b0c1e9d
Joshua Grant <grantjoshua1995@gmail.com>
2024-02-12 20:17:02 +0100

    added join logic

--- PizzaBot.py
+++ PizzaBot.py
@@ -8,29 +8,22 @@ load_dotenv('.env.local')
 class Bot(commands.Bot):
 
     def __init__(self):
-        # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
-        # prefix can be a callable, which returns a list of strings or a string...
-        # initial_channels can also be a callable which returns a list of strings...
         super().__init__(token=os.getenv('TWITCH_TOKEN'),
                          prefix='!', initial_channels=[os.getenv('TWITCH_CHANNEL_NAME')])
 
         self.raffle_queue = []
         self.arena_rotation = ["itsWiiland"]
-        self.is_raffle_opened = False
+        self.is_raffle_open = False
 
     async def event_ready(self):
-        # Notify us when everything is ready!
-        # We are logged in and ready to chat and use commands...
         print(f'Logged in as | {self.nick}')
         print(f'User id is | {self.user_id}')
 
     async def event_message(self, message):
-        # Messages with echo set to True are messages sent by the bot...
-        # For now we just want to ignore them...
+        # Messages with echo set to True are messages sent by the bot
         if message.echo:
             return
 
-        # Print the contents of our message to console...
         print(message.content)
 
         # Since we have commands and are overriding the default `event_message`
@@ -49,8 +42,8 @@ class Bot(commands.Bot):
     async def open(self, ctx: commands.Context):
         is_privileged_user = self.check_user_privilege(ctx.message)
 
-        if is_privileged_user and not self.is_raffle_opened:
-            self.is_raffle_opened = True
+        if is_privileged_user and not self.is_raffle_open:
+            self.is_raffle_open = True
             await ctx.send(f'The raffle is now open!')
         elif not is_privileged_user:
             await ctx.send(f'Sorry you cannot do that!')
@@ -61,8 +54,8 @@ class Bot(commands.Bot):
     async def close(self, ctx: commands.Context):
         is_privileged_user = self.check_user_privilege(ctx.message)
 
-        if is_privileged_user and self.is_raffle_opened:
-            self.is_raffle_opened = False
+        if is_privileged_user and self.is_raffle_open:
+            self.is_raffle_open = False
             await ctx.send(f'The raffle is now closed!')
         elif not is_privileged_user:
             await ctx.send(f'Sorry you cannot do that!')
@@ -71,13 +64,18 @@ class Bot(commands.Bot):
 
     @commands.command()
     async def join(self, ctx: commands.Context):
-        # Here we have a command hello, we can invoke our command with our prefix and command name
-        # e.g ?hello
-        # We can also give our commands aliases (different names) to invoke with.
+        user = ctx.author.name
 
-        # Send a hello back!
-        # Sending a reply back to the channel is easy... Below is an example.
-        await ctx.send(f'Hello {ctx.author.name}!')
+        if not self.is_raffle_open:
+            await ctx.send(f'The raffle is not open {user}!')
+            return
+
+        elif user in self.raffle_queue:
+            await ctx.send(f'{ctx.author.name} is already in the queue')
+            return
+
+        self.raffle_queue.append(user)
+        await ctx.send(f'{ctx.author.name} is added to the queue!')
 
 
 bot = Bot()
