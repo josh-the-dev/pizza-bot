08dc0031832201bce77f97614b4a90c40d074e35
Joshua Grant <grantjoshua1995@gmail.com>
2024-02-15 00:37:29 +0100

    add win streak logic. add/remove logic from arena

--- PizzaBot.py
+++ PizzaBot.py
@@ -15,6 +15,7 @@ class Bot(commands.Bot):
         self.raffle_queue = []
         self.arena_rotation = ["itsWiiland"]
         self.is_raffle_open = False
+        self.win_streak = 0
 
     async def event_ready(self):
         print(f'Logged in as | {self.nick}')
@@ -80,7 +81,9 @@ class Bot(commands.Bot):
 
     @commands.command()
     async def pick(self, ctx: commands.Context):
-
+        is_privileged_user = self.check_user_privilege(ctx.message)
+        if not is_privileged_user:
+            return
         if not self.is_raffle_open:
             await ctx.send(f'The raffle is not open!')
             return
@@ -92,6 +95,80 @@ class Bot(commands.Bot):
         self.arena_rotation.append(random_user)
         self.raffle_queue.remove(random_user)
         await ctx.send(f'{random_user} has been selected!')
+
+    @commands.command()
+    async def add(self, ctx: commands.Context):
+        is_privileged_user = self.check_user_privilege(ctx.message)
+        if not is_privileged_user:
+            return
+        split_message = ctx.message.content.split()
+        user_to_add = split_message[1]
+
+        if user_to_add in self.arena_rotation:
+            await ctx.send(f'{user_to_add} is already in the arena!')
+            return
+        if len(self.arena_rotation) == 6:
+            await ctx.send('The arena is already at 6 people!')
+            return
+        self.arena_rotation.append(user_to_add)
+        await ctx.send(f'{user_to_add} added to the arena!')
+
+    @commands.command()
+    async def remove(self, ctx: commands.Context):
+        is_privileged_user = self.check_user_privilege(ctx.message)
+        if not is_privileged_user:
+            return
+        split_message = ctx.message.content.split()
+        user_to_remove = split_message[1]
+
+        if user_to_remove not in self.arena_rotation:
+            await ctx.send(f'{user_to_remove} is not in the arena!')
+            return
+        if user_to_remove is self.arena_rotation[0]:
+            self.win_streak = 0
+        self.arena_rotation.remove(user_to_remove)
+        await ctx.send(f'{user_to_remove} removed from the arena!')
+
+    @commands.command()
+    async def win(self, ctx: commands.Context):
+        is_privileged_user = self.check_user_privilege(ctx.message)
+
+        if not is_privileged_user:
+            return
+
+        winning_user = self.arena_rotation[0]
+        losing_user = self.arena_rotation[1]
+        last_user = self.arena_rotation[4]
+        self.win_streak += 1
+
+        await ctx.send(f'@{losing_user} please leave the arena!')
+        await ctx.send(f'@{last_user} please join the arena!')
+        self.arena_rotation.append(self.arena_rotation.pop(1))
+
+        if self.win_streak is 3:
+            await ctx.send(f'@{winning_user} you won 3 games in a row, please leave the arena.')
+            self.win_streak = 0
+            self.arena_rotation.append(self.arena_rotation.pop(0))
+
+    @commands.command()
+    async def lose(self, ctx: commands.Context):
+        is_privileged_user = self.check_user_privilege(ctx.message)
+
+        if not is_privileged_user:
+            return
+
+        losing_user = self.arena_rotation[0]
+        last_user = self.arena_rotation[4]
+        self.win_streak += 1
+
+        await ctx.send(f'@{losing_user} please leave the arena!')
+        await ctx.send(f'@{last_user} please join the arena!')
+        self.arena_rotation.append(self.arena_rotation.pop(0))
+        self.win_streak = 0
+
+    @commands.command()
+    async def list(self, ctx: commands.Context):
+        await ctx.send(f'The arena list is the following: {", ".join(self.arena_rotation)}')
 
 
 bot = Bot()
